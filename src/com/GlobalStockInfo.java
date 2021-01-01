7ca8115af2773caecbd655c6889e96d977bdb4a3
daihual@cn.ibm.com <daihual@cn.ibm.com>
2021-01-01 16:35:47 +0800

    update stockinfo

--- src/com/GlobalStockInfo.java
+++ src/com/GlobalStockInfo.java
@@ -1,5 +1,10 @@
 package com;
 
+
+import com.bean.InfoBean;
+
+import java.util.List;
+
 /**
  * @Auther: neetriht scott
  * @Date: 1/1/2021 - 4:13 PM
@@ -8,4 +13,74 @@ package com;
  */
 
 public class GlobalStockInfo {
+
+    public static void printInfoBean(String stock_code, InfoBean info) {
+
+        System.out.println("股票代码：" + info.getSTOCK_CODE());
+        System.out.println("报告日期: " + info.getTRADING_DT());
+
+        System.out.println("所属地区：" + info.getLOCATION());
+        System.out.println("上市时间：" + info.getLIST_DT());
+        System.out.println("每股现金流(元)：" + info.getFCFPS());
+        System.out.println("净利润增长率(%)：" + info.getPRONW());
+
+        System.out.println("总股本：" + info.getEQUITY());
+        System.out.println("流通A股(亿)：" + info.getLIQUID());
+        System.out.println("每股公积金(元)：" + info.getEUPF());
+        System.out.println("每股未分配利润：" + info.getUPPS());
+
+        System.out.println("每股收益：" + info.getEPS());
+        System.out.println("每股净资产：" + info.getNAPS());
+        System.out.println("净资产收益率(%)：" + info.getROE());
+        System.out.println("主营收入增长率(%): " + info.getPIGR());
+
+        System.out.println("投资亮点: " + info.getDESC());
+    }
+
+    public static InfoBean setValue(List<String> l) {
+        InfoBean info = new InfoBean();
+        info.setSTOCK_CODE(l.remove(0));  // 股票代码
+        info.setTRADING_DT(l.remove(0));  // 报告日期
+        info.setLOCATION(l.remove(0));    // 所属地区
+        info.setLIST_DT(l.remove(0));     // 上市时间
+        info.setFCFPS(l.remove(0));       // 每股现金流(元)
+        info.setPRONW(l.remove(0));       // 净利润增长率(%)
+
+        info.setEQUITY(l.remove(0));      // 总股本
+        info.setLIQUID(l.remove(0));      // 流通A股(亿)
+        info.setEUPF(l.remove(0));        // 每股公积金(元)
+        info.setUPPS(l.remove(0));        // 每股未分配利润
+
+        info.setEPS(l.remove(0));         // 每股收益
+        info.setNAPS(l.remove(0));        // 每股净资产
+        info.setROE(l.remove(0));         // 净资产收益率(%)
+        info.setPIGR(l.remove(0));        // 主营收入增长率(%)
+
+        info.setDESC(l.remove(0));        // 投资亮点
+        return info;
+    }
+
+
+//    public static void printInfoBean(String stock_code, InfoBean info) {
+//
+//        System.out.println("股票代码：" + info.getSTOCK_CODE());
+//        System.out.println("报告日期: " + info.getTRADING_DT());
+//
+//        System.out.println("所属地区：" + info.getLOCATION());
+//        System.out.println("上市时间：" + info.getLIST_DT());
+//        System.out.println("每股现金流(元)：" + info.getFCFPS());
+//        System.out.println("净利润增长率(%)：" + info.getPRONW());
+//
+//        System.out.println("总股本：" + info.getEQUITY());
+//        System.out.println("流通A股(亿)：" + info.getLIQUID());
+//        System.out.println("每股公积金(元)：" + info.getEUPF());
+//        System.out.println("每股未分配利润：" + info.getUPPS());
+//
+//        System.out.println("每股收益：" + info.getEPS());
+//        System.out.println("每股净资产：" + info.getNAPS());
+//        System.out.println("净资产收益率(%)：" + info.getROE());
+//        System.out.println("主营收入增长率(%): " + info.getPIGR());
+//
+//        System.out.println("投资亮点: " + info.getDESC());
+//    }
 }
---
+++ src/com/bean/InfoBean.java
@@ -0,0 +1,142 @@
+package com.bean;
+
+public class InfoBean {
+
+    String STOCK_CODE;
+    String LOCATION;
+    String EQUITY;
+    String EPS;
+    String LIST_DT;
+    String LIQUID;
+    String NAPS;
+    String FCFPS;
+    String EUPF;
+    String ROE;
+    String PRONW;
+    String UPPS;
+    String PIGR;
+    String TRADING_DT;
+    String DESC;
+
+    public String getSTOCK_CODE() {
+        return STOCK_CODE;
+    }
+
+    public void setSTOCK_CODE(String STOCK_CODE) {
+        this.STOCK_CODE = STOCK_CODE;
+    }
+
+    public String getDESC() {
+        return DESC;
+    }
+
+    public void setDESC(String DESC) {
+        this.DESC = DESC;
+    }
+
+    public String getLOCATION() {
+        return LOCATION;
+    }
+
+    public void setLOCATION(String LOCATION) {
+        this.LOCATION = LOCATION;
+    }
+
+    public String getEQUITY() {
+        return EQUITY;
+    }
+
+    public void setEQUITY(String EQUITY) {
+        this.EQUITY = EQUITY;
+    }
+
+    public String getEPS() {
+        return EPS;
+    }
+
+    public void setEPS(String EPS) {
+        this.EPS = EPS;
+    }
+
+    public String getLIST_DT() {
+        return LIST_DT;
+    }
+
+    public void setLIST_DT(String LIST_DT) {
+        this.LIST_DT = LIST_DT;
+    }
+
+    public String getLIQUID() {
+        return LIQUID;
+    }
+
+    public void setLIQUID(String LIQUID) {
+        this.LIQUID = LIQUID;
+    }
+
+    public String getNAPS() {
+        return NAPS;
+    }
+
+    public void setNAPS(String NAPS) {
+        this.NAPS = NAPS;
+    }
+
+    public String getFCFPS() {
+        return FCFPS;
+    }
+
+    public void setFCFPS(String FCFPS) {
+        this.FCFPS = FCFPS;
+    }
+
+    public String getEUPF() {
+        return EUPF;
+    }
+
+    public void setEUPF(String EUPF) {
+        this.EUPF = EUPF;
+    }
+
+    public String getROE() {
+        return ROE;
+    }
+
+    public void setROE(String ROE) {
+        this.ROE = ROE;
+    }
+
+    public String getPRONW() {
+        return PRONW;
+    }
+
+    public void setPRONW(String PRONW) {
+        this.PRONW = PRONW;
+    }
+
+    public String getUPPS() {
+        return UPPS;
+    }
+
+    public void setUPPS(String UPPS) {
+        this.UPPS = UPPS;
+    }
+
+    public String getPIGR() {
+        return PIGR;
+    }
+
+    public void setPIGR(String PIGR) {
+        this.PIGR = PIGR;
+    }
+
+    public String getTRADING_DT() {
+        return TRADING_DT;
+    }
+
+    public void setTRADING_DT(String TRADING_DT) {
+        this.TRADING_DT = TRADING_DT;
+    }
+
+
+}
